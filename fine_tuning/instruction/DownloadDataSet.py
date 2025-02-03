430b22550a278cb469934a73807deea3ddbb5862
wumingyang <352032370@qq.com>
2025-02-03 14:19:55 +0800

    下载指令微调需要的数据

---
+++ fine_tuning/instruction/DownloadDataSet.py
@@ -0,0 +1,30 @@
+import json
+import os
+import urllib
+
+
+def download_and_load_file(file_path, url):
+    # Download and save the file if it doesn't exist
+    if not os.path.exists(file_path):
+        with urllib.request.urlopen(url) as response:
+            text_data = response.read().decode("utf-8")
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text_data)
+
+    else:
+        print(f"{file_path} already exists. Skipping download.")
+
+    # Load the file
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return data
+
+
+file_path = "instruction-data.json"
+url = (
+    "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch"
+    "/main/ch07/01_main-chapter-code/instruction-data.json")
+
+data = download_and_load_file(file_path, url)
+print("Number of entries:", len(data))
